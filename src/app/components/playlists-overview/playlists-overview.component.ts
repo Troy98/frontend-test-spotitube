90fad840f9873ea17dce8cc6fbb3449609d5ed16
Luuk Lentjes <luuklentjes2002@gmail.com>
2023-03-29 20:39:44 +0200

    .

--- src/app/components/playlists-overview/playlists-overview.component.ts
+++ src/app/components/playlists-overview/playlists-overview.component.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-playlists-overview',
   templateUrl: './playlists-overview.component.html',
   styleUrls: ['./playlists-overview.component.scss']
 })
-export class PlaylistOverviewComponent {
+export class PlaylistOverviewComponent implements OnInit{
 
   constructor() {
   }
+
+  ngOnInit(): void {
+    this.clearAccessTokenFromUrl();
+  }
+
+  clearAccessTokenFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.delete('accessToken');
+    const newUrl = window.location.origin + window.location.pathname + '?' + urlParams.toString();
+    window.history.replaceState({}, '', newUrl);
+  }
+
+
 }
